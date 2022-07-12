638da0c49219a47e51d7af4790c0bd1c03efa17f
Manu Radhakrishnan <manuvr999@gmail.com>
2022-07-12 15:59:10 +0530

    Add files via upload
    Initial Code Push

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using NEventStorePOC.Model;
+using NEventStorePOC.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDBSettings"));
+builder.Services.AddSingleton<MongoDBService>();
+
+builder.Services.Configure<SQLDBSettings>(builder.Configuration.GetSection("SQLDBSettings"));
+builder.Services.AddSingleton<SQLDBService>();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
